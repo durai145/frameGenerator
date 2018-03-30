3b50d8a7be69b35fc3dbc30b779686e5be511ddd
duraimurugan Govindaraj <durai@duraimuugansAir.fios-router.home>
2018-03-30 19:08:34 -0400

    first commit

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# frameGenerator" >> README.md
+git init
+git add --all
+git commit -m "first commit"
+git remote add origin https://github.com/durai145/frameGenerator.git
+git push -u origin master
